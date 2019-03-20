78a577929d76fd504c087572a8807f0525737319
Hacker719 <neel.redkar@gmail.com>
2019-03-20 16:22:33 -0700

    Initial Commit

---
+++ TaskManager/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TaskManager
+//
+//  Created by Neel Redkar on 3/20/19.
+//  Copyright © 2019 tecton. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TaskManager/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TaskManager
+//
+//  Created by Neel Redkar on 3/20/19.
+//  Copyright © 2019 tecton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
